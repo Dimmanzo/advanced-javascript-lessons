2212f87d897a0828e320af0331e9ac2b619a00a6
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-07-20 11:33:36 +0000

    Added ...rest

--- training.js
+++ training.js
@@ -67,3 +67,29 @@ console.log(obj3)
 // Copying only part of an array/object
 let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
 console.log(arr5)
+
+
+// ------------------------------------------- Spread //
+
+
+// Regular function call 
+const sumALL = (a, b, c) => a + b + c;
+let sum = sumALL(2, 2, 2);
+console.log("sum", sum);
+
+// Extra arguments are ignored
+let sum2 = sumALL(1, 2, 3, 4, 5, 6);
+console.log("sum2", sum2);
+
+// Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("sum3", sum3)
