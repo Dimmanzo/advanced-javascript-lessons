4579bf2170cbac142a1e824d76cf1ae575caf971
Dmitrij Sazniov <dmitrij.sazniov@gmail.com>
2024-07-20 11:37:49 +0000

    Added destructuring

--- training.js
+++ training.js
@@ -31,7 +31,7 @@ sayHello();
 // Returning Multiple Lines
 
 const returnMultipleLines = () => (
-  `<p>
+    `<p>
     This is a multiline string!
   </p >`
 )
@@ -57,15 +57,15 @@ console.log(arr3)
 console.log(arr4)
 
 // Copying an object
-let obj1 = {a:1, b: 2, c: 3};
-let obj2 = {...obj1, d: 4};
-let obj3 = {...obj1, b: 5};
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = { ...obj1, d: 4 };
+let obj3 = { ...obj1, b: 5 };
 console.log(obj1)
 console.log(obj2)
 console.log(obj3)
 
 // Copying only part of an array/object
-let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+let arr5 = [...arr1, { ...obj1 }, ...arr3, "x", "y", "z"];
 console.log(arr5)
 
 
@@ -93,3 +93,60 @@ const sumRest = (a, b, c, ...rest) => {
 
 let sum3 = sumRest(1, 2, 3, 4, 5, 6);
 console.log("sum3", sum3)
+
+
+// ------------------------------------------- Destructuring //
+
+
+// Destructuring arrays
+let ages = [30, 26, 27];
+// let john = ages[0];
+// let mary = ages[1];
+// let joe = ages[2];
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
+
+// Destructuring objects
+let jobs = {
+    mike: "designer",
+    jill: "developer",
+    alicia: "accountant",
+};
+let { mike, jill, alicia } = jobs;
+console.log(mike, jill, alicia);
+
+// Destructuring subsets
+let languages = ["english", "french", "spanish", "german", "japanese"];
+let [johnNative, johnSecondary] = languages;
+console.log(johnNative, johnSecondary);
+
+let [, , maryNative, marySecondary] = languages;
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: "english",
+    secondLanguage: "french",
+    thirdLanguage: "german",
+    fourthLanguage: "japanese",
+};
+
+let { firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
+
+// Using rest parameter syntax
+let fruits = ["apple", "orange", "banana", "peach", "cherry"];
+let [favorite, secondFavorite, ...others] = fruits;
+console.log(favorite);
+console.log(secondFavorite);
+console.log(...others);
+
+let favoriteFoods = {
+    brian: "pizza",
+    anna: "pasta",
+    sarah: "vegetarian",
+    andrea: "steak",
+};
+let { brian, anna, ...rest } = favoriteFoods;
+console.log(brian);
+console.log(anna);
+console.log(rest);
